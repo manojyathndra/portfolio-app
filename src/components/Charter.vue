c6375c4994f5d684bbe014ec7b78917f52814a32
Manoj Yathindra Thagadur Srinivasu <manoj.yathindra@gmail.com>
2024-03-07 11:51:43 -0700

    about changes

--- src/components/Charter.vue
+++ src/components/Charter.vue
@@ -1,48 +1,15 @@
 <template>
-   <div class="flex.container">
-    <div class="flex-sixty">
-      <div class="video-background">
-        <v-container>
-            <video autoplay muted loop id="video-bg">
-                <source src="@/assets/Charter.mp4" type="video/mp4">
-                Your browser does not support the video tag.
-            </video>
-        </v-container>
-      </div>
-    </div>
-    <div class="flex-forty">
-      <h1 class="text-h4 font-weight-thin mb-4">
-        Vuetify
-      </h1>
-      <h4 class="subheading">
-        Build your application today!
-      </h4>
-    </div>
+  <div>
+    <v-container>
+        <video autoplay muted loop id="video-bg">
+            <source src="@/assets/Charter.mp4" type="video/mp4">
+            Your browser does not support the video tag.
+        </video>
+    </v-container>
   </div>
 </template>
 
 <style>
-.flex-container {
-  display: flex;
-}
-
-.flex-item-60 {
-  flex: 60;
-}
-
-.flex-item-40 {
-  flex: 40;
-}
-.video-background {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 60%;
-  height: 100%;
-  overflow: hidden;
-  z-index: -1;
-}
-
 #video-bg {
   position: absolute;
   top: 50%;
